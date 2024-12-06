7f257f44154f4db38c22b828d6b596eaebe095a4
const <jake@bittensor.com>
2024-12-06 02:48:08 +0000

    update and restart

--- test_auto.py
+++ test_auto.py
@@ -1,10 +1,17 @@
 
 
+import sys
 import time
 from tplr import tplr
 
-SPEC_VERSION = 0 # Run version.
+SPEC_VERSION = tplr.get_remote_spec_version() # Run version.
 
 while True:
-    print( tplr.get_pm2_process_name(), tplr.get_remote_spec_version() )
+    new_spec = tplr.get_remote_spec_version()
+    print( tplr.get_pm2_process_name(), new_spec )
+    if SPEC_VERSION != new_spec:
+        print ('restart')
+        time.sleep(5)
+        tplr.update_and_restart()
+        sys.exit(0)
     time.sleep(10)
